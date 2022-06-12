4f9deec283c1a0de1a1dc54aa961bbc622c4a286
DanyaBobreshov <dbober94@gmail.com>
2022-06-12 23:40:45 +0300

    Initial commit

---
+++ src/main/java/com/example/controlTos/ControlTosApplication.java
@@ -0,0 +1,13 @@
+package com.example.controlTos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ControlTosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ControlTosApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/controlTos/ControlTosApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.controlTos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ControlTosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
